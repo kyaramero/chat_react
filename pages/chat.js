3240b915ddc6daccaab1f2f5af5e8bf9c11b8b01
Júlia Andrade <julia.b.andrade@outlook.com>
2022-01-31 15:49:42 -0300

    day 3

--- pages/chat.js
+++ pages/chat.js
@@ -1,5 +1,228 @@
-export default function ChatPage(){
-    return(
-        <div>Chat</div>
+import { Box, Text, TextField, Image, Button } from '@skynexui/components';
+import React from 'react';
+import appConfig from '../config.json';
+import { createClient } from '@supabase/supabase-js'
+
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzY0MjkyMywiZXhwIjoxOTU5MjE4OTIzfQ.jbeD9NUX-i0kOanwx6b8t5FwaoOXgjj72BTxHSFc5v8';
+const SUPABASE_URL = 'https://lbragtgyhfebstuxocyv.supabase.co';
+const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+//const dadosSupabase = supabaseClient.from('messages').select('*');
+
+
+export default function ChatPage() {
+    const [message, setMessage] = React.useState('');
+    const [chatList, setchatList] = React.useState([]);
+
+    React.useEffect(() => {
+        supabaseClient
+          .from('messages')
+          .select('*')
+          .order('id', { ascending: false })
+          .then(({ data }) => {
+            console.log('Dados da consulta:', data);
+            setchatList(data);
+          });
+      }, []);
+    
+      function handleNewMessage(newMessage) {
+        const message = {
+          // id: chatList.length + 1,
+          de: 'kyaramero',
+          text: newMessage,
+        };
+    
+        supabaseClient
+          .from('messages')
+          .insert([
+            // Tem que ser um objeto com os MESMOS CAMPOS que você escreveu no supabase
+            message
+          ])
+          .then(({ data }) => {
+            console.log('Criando mensagem: ', data);
+            setchatList([
+              data[0],
+              ...chatList,
+            ]);
+          });
+    
+        setMessage('');
+      }
+    
+      return (
+        <Box
+            styleSheet={{
+                display: 'flex', alignItems: 'center', justifyContent: 'center',
+                backgroundColor: appConfig.theme.colors.primary[500],
+                backgroundImage: `url(https://wallpapercave.com/wp/wp2757874.gif)`,
+                backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
+                color: appConfig.theme.colors.neutrals['000']
+            }}
+        >
+            <Box
+                styleSheet={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    flex: 1,
+                    boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
+                    borderRadius: '5px',
+                    backgroundColor: appConfig.theme.colors.neutrals[700],
+                    height: '100%',
+                    maxWidth: '95%',
+                    maxHeight: '95vh',
+                    padding: '32px',
+                }}
+            >
+                <Header />
+                <Box
+                    styleSheet={{
+                        position: 'relative',
+                        display: 'flex',
+                        flex: 1,
+                        height: '80%',
+                        backgroundColor: appConfig.theme.colors.neutrals[600],
+                        flexDirection: 'column',
+                        borderRadius: '5px',
+                        padding: '16px',
+                    }}
+                >
+
+                    <MessageList messages={chatList} />
+
+                    <Box
+                        as="form"
+                        styleSheet={{
+                            display: 'flex',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <TextField
+                            placeholder="Insira sua mensagem aqui..."
+                            value={message}
+                            onChange={(event) => {
+                                const valor = event.target.value;
+                                setMessage(valor);
+                            }}
+                            onKeyPress={(event) => {
+                                if(event.key === 'Enter'){
+                                    event.preventDefault();
+                                    handleNewMessage(message);
+                                }
+                            }}
+                            type="textarea"
+                            styleSheet={{
+                                width: '100%',
+                                border: '0',
+                                resize: 'none',
+                                borderRadius: '5px',
+                                padding: '6px 8px',
+                                backgroundColor: appConfig.theme.colors.neutrals[800],
+                                marginRight: '12px',
+                                color: appConfig.theme.colors.neutrals[200],
+                            }}
+                        />
+                        <Button
+                                type='submit'
+                                label='➜'
+                                onClick={(evt) => {
+                                    evt.preventDefault();
+                                    handleNewMessage(message);
+                                  }}
+                                width='50%'
+                                buttonColors={{
+                                contrastColor: appConfig.theme.colors.neutrals["000"],
+                                mainColor: appConfig.theme.colors.primary[500],
+                                mainColorLight: appConfig.theme.colors.primary[400],
+                                mainColorStrong: appConfig.theme.colors.primary[600],
+                                }}
+                            />
+                    </Box>
+                </Box>
+            </Box>
+        </Box>
+    )
+}
+
+function Header() {
+    return (
+        <>
+            <Box styleSheet={{ width: '100%', marginBottom: '16px', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }} >
+                <Text variant='heading5'>
+                    Chat
+                </Text>
+                <Button
+                    variant='tertiary'
+                    colorVariant='neutral'
+                    label='Logout'
+                    href="/"
+                />
+            </Box>
+        </>
+    )
+}
+
+function MessageList(props) {
+    console.log(props);
+    return (
+        <Box
+            tag="ul"
+            styleSheet={{
+                overflow: 'auto',
+                display: 'flex',
+                flexDirection: 'column-reverse',
+                flex: 1,
+                color: appConfig.theme.colors.neutrals["000"],
+                marginBottom: '16px',
+            }}
+        >
+            {props.messages.map((message) => {
+                return (
+                    <Text
+                        key={message.id}
+                        tag="li"
+                        styleSheet={{
+                            borderRadius: '5px',
+                            padding: '6px',
+                            marginBottom: '12px',
+                            hover: {
+                                backgroundColor: appConfig.theme.colors.neutrals[700],
+                            }
+                        }}
+                    >
+                        <Box
+                            styleSheet={{
+                                marginBottom: '8px',
+                                display: 'flex',
+                            }}
+                        >
+                            <Image
+                                styleSheet={{
+                                    width: '20px',
+                                    height: '20px',
+                                    borderRadius: '50%',
+                                    display: 'inline-block',
+                                    marginRight: '8px',
+                                }}
+                                src={`https://github.com/${message.de}.png`}
+                            />
+                            <Text tag="strong">
+                                {message.de}
+                            </Text>
+                            <Text
+                                styleSheet={{
+                                    fontSize: '10px',
+                                    marginLeft: '8px',
+                                    color: appConfig.theme.colors.neutrals[300],
+                                }}
+                                tag="span"
+                            >
+                                {(new Date().toLocaleDateString())}
+                            </Text>
+                        </Box>
+                        {message.text}
+                    </Text>
+                );
+            })}
+        </Box>
     )
 }
